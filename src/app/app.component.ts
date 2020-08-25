d5e5d645f1c4db2b10a0246863f43ecfd3c1fdb6
MUHAMMAD RIZKI <U069118@bca>
2020-08-25 17:59:51 +0700

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked } from "@angular/core";
+
+declare let paypal: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+
+  templateUrl: "./app.component.html",
+
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'paypal-angular';
+export class AppComponent implements AfterViewChecked {
+  addScript: boolean = false;
+
+  paypalLoad: boolean = true;
+
+  finalAmount: number = 1;
+
+  paypalConfig = {
+    env: "sandbox",
+
+    client: {
+      sandbox:
+        "ATcMlsvLbJ6V0_MEUsxkQzun-9FpMFK7MUDJaZHbt_7YtqeS-XZBfPeGJkQha3UEPivhKqdQoeQ4LFV_",
+
+      production: "<your-production-key-here>",
+    },
+
+    commit: true,
+
+    payment: (data, actions) => {
+      return actions.payment.create({
+        payment: {
+          transactions: [
+            { amount: { total: this.finalAmount, currency: "USD" } },
+          ],
+        },
+      });
+    },
+
+    onAuthorize: (data, actions) => {
+      return actions.payment.execute().then((payment) => {
+        //Do something when payment is successful.
+      });
+    },
+  };
+
+  ngAfterViewChecked(): void {
+    if (!this.addScript) {
+      this.addPaypalScript().then(() => {
+        paypal.Button.render(this.paypalConfig, "#paypal-checkout-btn");
+
+        this.paypalLoad = false;
+      });
+    }
+  }
+
+  addPaypalScript() {
+    this.addScript = true;
+
+    return new Promise((resolve, reject) => {
+      let scripttagElement = document.createElement("script");
+
+      scripttagElement.src = "https://www.paypalobjects.com/api/checkout.js";
+
+      scripttagElement.onload = resolve;
+
+      document.body.appendChild(scripttagElement);
+    });
+  }
 }
